0aa61980d2f9133400ca8c1710c035f0cf1ea47c
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-31 20:00:45 -0500

    header

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -1,34 +1,70 @@
-import React from 'react';
-import './styles/Header.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
 
-const pages = [
-    {href: "about", pageName: "About"},
-    {href: "projects", pageName: "Projects"},
-    {href: "contact", pageName: "Contact"},
-    {href: "resume", pageName: "Resume"}
-];
+import Auth from '../utils/auth';
 
-function Header({ currentPage, handlePageChange }) {
+const AppNavbar = () => {
+  // set modal display state
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className='header'>
-    <ul className="nav nav-tabs">
-    {pages.map(p => (
-        <li className="nav-item">
-        <a
-          href={`#${p.href}`}
-          onClick={() => handlePageChange(p.pageName)}
-          className={currentPage === p.pageName ? 'nav-link active' : 'nav-link'}
-        >
-          {p.pageName}
-        </a>
-      </li>
-    )
-    )
-    }
-    </ul>
-    </div>
+    <>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Container fluid>
+          <Navbar.Brand as={Link} to='/'>
+            DevList
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbar' />
+          <Navbar.Collapse id='navbar'>
+            <Nav className='ml-auto'>
+              {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* set modal data up */}
+      <Modal
+        size='lg'
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby='signup-modal'>
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <SignUpForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
   );
-}
+};
 
-export default Header;
+export default AppNavbar;
