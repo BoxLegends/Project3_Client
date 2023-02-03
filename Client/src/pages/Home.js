9c63ea7edd7f27d9d4e9cd9a671b4a7703bdb78c
jwill5432 <110855104+jwill5432@users.noreply.github.com>
2023-02-02 19:39:58 -0500

    <adding home>

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Home() {
     return (
-      <div className='home'>
-
+      <div style={{textAlign: `center`}} className='home'>
+           <div>
+      <h1 style={{marginTop: `10px`, marginBottom: `10px`}}>Welcome to Our Dev Team!</h1>
+      <img src="Client\public\officebuildingstock.jpg"/>
+      <p>Discover the members of our talented development team and learn more about their skills and experience!</p>
+    </div>
       </div>
     );
   }
