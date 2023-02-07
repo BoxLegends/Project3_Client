c1e7b6566e807b90386026bf75fea02ba9885434
Nick Burns <nick_burns11@hotmail.com>
2023-02-07 14:34:19 -0500

    falvor text

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -8,7 +8,20 @@ export default function Home() {
            <header className="title">
       <h1>Welcome to Devs List!</h1>
       </header>
-      <p>Discover the members of our talented development team and learn more about their skills and experience!</p>
+      <h1>Our developers</h1>
+           <p> Visit our team one page to Discover the members of our talented development team and learn more about their skills and experience! We also offer a system 
+        for users to upload themselves to the website in order to get their name out there and display many of their talents as well.
+          </p>
+        <h1> The Mission</h1>
+          <p> Here at devs list we believe that collaboration is key and we want to make collaboration easier! our website sort of acts as a roster in order to
+        easily find other like minded talented developers to work with! Sign up today and start collaborating more effectively, and add a profile so
+        its easier for others to find you!
+         </p>
+         <aside>
+          <p>
+            
+          </p>
+         </aside>
     </div>
       </div>
     );
