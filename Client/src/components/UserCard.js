00d0277544f62f654144439fb8c9355002b3ebd0
JaredIng87 <jaredingersoll14@gmail.com>
2023-02-07 15:21:07 -0500

    edit style

--- Client/src/components/UserCard.js
+++ Client/src/components/UserCard.js
@@ -3,10 +3,11 @@ import './styles/UserCard.css';
 import Default from '../images/default.png';
 import Yorlan from '../images/idk.jpg';
 import Jared from '../images/self.jpg';
+import Nick from '../images/Nick.jpg';
 
 
   const members = [
-    {url: "users/63e1ada3f632ba6ebf048bf2", name: "Nick", title: "developer", src: Default},
+    {url: "users/63e1ada3f632ba6ebf048bf2", name: "Nick", title: "developer", src: Nick},
     {url: "users/63e1ada3f632ba6ebf048bf3", name: "Jared", title: "Developer", src: Jared},
     {url: "users/63e1ada3f632ba6ebf048bf4", name: "John", title: "Developer", src:Default},
     {url: "users/63e1ada3f632ba6ebf048bf5", name: "Sebastian", title: "Developer", src: Default},
--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -4,25 +4,24 @@ import './styles/Home.css';
 export default function Home() {
     return (
       <div className='home'>
-           <div>
+           
            <header className="title">
       <h1>Welcome to Devs List!</h1>
       </header>
-      <h1>Our developers</h1>
-           <p> Visit our team one page to Discover the members of our talented development team and learn more about their skills and experience! We also offer a system 
+      <div className='content-container'>
+           <p> <h1>Our developers</h1><br></br> Visit our team one page to Discover the members of our talented development team and learn more about their skills and experience! We also offer a system 
         for users to upload themselves to the website in order to get their name out there and display many of their talents as well.
           </p>
-        <h1> The Mission</h1>
-          <p> Here at devs list we believe that collaboration is key and we want to make collaboration easier! our website sort of acts as a roster in order to
+
+          <p> <h1> The Mission</h1><br></br>Here at devs list we believe that collaboration is key and we want to make collaboration easier! our website sort of acts as a roster in order to
         easily find other like minded talented developers to work with! Sign up today and start collaborating more effectively, and add a profile so
         its easier for others to find you!
          </p>
          <aside>
-          <p>
-            
-          </p>
+          dkjfkds
          </aside>
+         </div>
     </div>
-      </div>
+      
     );
   }
