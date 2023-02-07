e071ea5a20f10e3a6df5211aa9ed4550a0e599fb
Nick Burns <nick_burns11@hotmail.com>
2023-02-07 15:29:46 -0500

    hero banner

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -12,6 +12,8 @@ export default function Home() {
            <p> Visit our team one page to Discover the members of our talented development team and learn more about their skills and experience! We also offer a system 
         for users to upload themselves to the website in order to get their name out there and display many of their talents as well.
           </p>
+
+        <img className = "hero" src ="https://i.ytimg.com/vi/-7vd6wswQyU/maxresdefault.jpg"></img> 
         <h1> The Mission</h1>
           <p> Here at devs list we believe that collaboration is key and we want to make collaboration easier! our website sort of acts as a roster in order to
         easily find other like minded talented developers to work with! Sign up today and start collaborating more effectively, and add a profile so
